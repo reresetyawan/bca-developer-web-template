a60a5c50f5503fcdad9ad10df3737b88a64a8267
reresetyawan <rere_setyawan@yahoo.co.uk>
2019-07-17 09:41:14 +0700

    Start Project

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html dir="ltr" class="com_apiportal " lang="en-gb"><head>
+<link href="templates/css/css.css" rel="stylesheet" type="text/css">
+	<!-- base href="https://developer.bca.co.id/" -->
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="generator" content="Joomla! - Open Source Content Management">
+	<title>Home</title>
+	<link href="https://developer.bca.co.id/templates/purity_iii/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+	<link href="templates/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/style.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/swiper.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/dark.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/font-icons.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/animate.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/magnific-popup.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/responsive.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/colors.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/mobile.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/apiportal.css" rel="stylesheet" type="text/css">
+	<link href="templates/css/blog.css" rel="stylesheet" type="text/css">
+	<script async="" src="templates/js/analytics.js"></script><script src="templates/js/jquery_004.js" type="text/javascript"></script>
+	<script src="templates/js/jquery-noconflict.js" type="text/javascript"></script>
+	<script src="templates/js/jquery-migrate.js" type="text/javascript"></script>
+	<script src="templates/js/jquery_003.js" type="text/javascript"></script>
+	<script src="templates/js/jquery.js" type="text/javascript"></script>
+	<script src="templates/js/jquery_002.js" type="text/javascript"></script>
+	<script src="templates/js/jquery_005.js" type="text/javascript"></script>
+	<script src="templates/js/highcharts.js" type="text/javascript"></script>
+	<script src="templates/js/apiportal.js" type="text/javascript"></script>
+
+			<!-- META FOR IOS & HANDHELD -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<style type="text/stylesheet">
+		@-webkit-viewport   { width: device-width; }
+		@-moz-viewport      { width: device-width; }
+		@-ms-viewport       { width: device-width; }
+		@-o-viewport        { width: device-width; }
+		@viewport           { width: device-width; }
+	</style>
+	<script type="text/javascript">
+		//<![CDATA[
+		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+			var msViewportStyle = document.createElement("style");
+			msViewportStyle.appendChild(
+				document.createTextNode("@-ms-viewport{width:auto!important}")
+			);
+			document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+		}
+		//]]>
+	</script>
+<meta name="HandheldFriendly" content="true">
+<meta name="apple-mobile-web-app-capable" content="YES">
+<!-- //META FOR IOS & HANDHELD -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="google-site-verification" content="">
+<style id="fit-vids-style">.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}</style></head>
+
+<body class="stretched device-lg">
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix" style="animation-duration: 1.5s; opacity: 1;">
+
+				
+
+		
+			
+						<div id="system-message-container">
+	</div>
+
+						<div id="home" class="page-section" style="position:absolute;top:0;left:0;width:100%;height:200px;z-index:-2;"></div>
+
+<section id="slider" class="slider-parallax full-screen with-header swiper_wrapper clearfix slider-parallax-visible" style="height: 678px;">
+
+	<div class="slider-parallax-inner" style="height: 678px; transform: translateY(0px);">
+
+		<div class="swiper-container swiper-parent swiper-container-horizontal" style="cursor: grab;">
+			<div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
+				<div class="swiper-slide dark swiper-slide-active" style="background-image: url(&quot;./templates/img/code.jpg&quot;); height: 678px; width: 1583px;">
+					<div class="video-overlay" style="background-color: rgba(38, 38, 38, 0.85);"></div> 
+				<div class="swiper-slide dark" style="height: 678px;">
+					<div class="container clearfix">
+						<div class="slider-caption slider-caption-center" style="transform: translateY(0px); opacity: 1; top: 251px;">
+							<h2 data-caption-animate="fadeInUp" class="fadeInUp animated">API <span class="BCAHome" id="BCAHome">BCA</span></h2>
+							<p data-caption-animate="fadeInUp" data-caption-delay="200" id="LayananPerbankan" class="fadeInUp animated">Layanan perbankan dalam aplikasi Anda</p>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div></section>
+
+<!-- Header
+============================================= -->
+<header id="header" class="full-header">
+
+	<div id="header-wrap" class="">
+
+		<div class="container clearfix">
+
+			<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+			<!-- Logo
+			============================================= -->
+			<div id="logo">
+				<a href="https://developer.bca.co.id/" class="standard-logo" data-dark-logo="components/com_apiportal/assets/imgportal/logo-dark.png"><img id="GambarLogo" src="templates/img/logobca.png" alt="Canvas Logo"></a>
+				<a href="https://developer.bca.co.id/" class="retina-logo" data-dark-logo="components/com_apiportal/assets/imgportal/logo-dark@2x.png"><img id="GambarLogo" src="templates/img/logobca.png" alt="Canvas Logo"></a>
+			</div><!-- #logo end -->
+
+			<!-- Primary Navigation
+			============================================= -->
+			<nav id="primary-menu">
+
+				<!-- START: Modules Anywhere --><ul class="one-page-menu sf-js-enabled" data-easing="easeInOutExpo" data-speed="1500" style="touch-action: pan-y;">
+<li class="item-101 active"><a href="https://developer.bca.co.id/">Home</a></li><li class="item-154"><a href="https://developer.bca.co.id/apis">APIs</a></li><li class="item-159">	<a href="https://developer.bca.co.id/documentation/" target="_blank">Documentation</a></li><li class="item-155"><a href="https://developer.bca.co.id/pricing">Pricing</a></li><li class="item-137"><a href="https://developer.bca.co.id/sign-in">Sign In</a></li></ul>
+<!-- END: Modules Anywhere -->
+
+				<!-- Top Search
+				============================================= -->
+			
+
+			</nav><!-- #primary-menu end -->
+
+		</div>
+
+	</div>
+
+</header><!-- #header end -->
+
+<div class="clear"></div>
+
+<!-- Content
+============================================= -->
+<section id="content" style="margin-bottom: 0px;">
+
+	<div class="content-wrap" style="padding:80px 0">
+	
+		<section id="section-about" class="page-section">
+
+			<div class="container clearfix">
+
+			<div class="container clearfix">
+
+				<div class="heading-block center">
+					<h2><font style="font-family: sans-serif">3</font> Langkah mudah menggunakan API BCA</h2>
+				</div>
+
+				<div class="col_one_third">
+					<div class="feature-box fbox-center fbox-dark fbox-plain ApiHome">
+						<div class="fbox-icon">
+							<img src="templates/img/explore.png" alt="Explore">
+						</div>
+						<h3>Telusuri</h3>
+						<p id="ApiHome">Kami hadir menjawab kebutuhan Anda akan solusi 
+layanan perbankan langsung dalam aplikasi Anda. Dalami lebih lanjut 
+solusi API yang siap kami bawa untuk Anda.</p>
+					</div>
+				</div>
+			<div class="col_one_third">
+				<div class="feature-box fbox-center fbox-dark fbox-plain ApiHome">
+					<div class="fbox-icon">
+						<img src="templates/img/integrate.png" alt="Integrate">
+					</div>
+					<h3>Integrasi</h3>
+					<p id="ApiHome">Coba langsung mengintegrasikan aplikasi Anda dengan
+ sistem BCA (development) melalui sandbox yang sudah kami persiapkan 
+khusus untuk Anda.</p>
+				</div>
+			</div>
+			<div class="col_one_third col_last">
+				<div class="feature-box fbox-center fbox-dark fbox-plain ApiHome">
+					<div class="fbox-icon">
+						<img src="templates/img/launch.png" alt="Integrate">
+					</div>
+					<h3>Implementasi</h3>
+					<p id="ApiHome">Ini saatnya Anda untuk menikmati layanan perbankan 
+yang mudah, aman dan nyaman dalam aplikasi Anda. Untuk kerjasama lebih 
+lanjut, <a href="https://developer.bca.co.id/contact-us">hubungi kami</a>.</p>
+				</div>
+			</div>
+
+			</div>
+
+
+				<div class="clear"></div>
+
+			</div>
+
+
+
+		</section>
+		
+		<div class="section">
+
+			<div class="container clearfix">
+
+				<div id="section-buy" class="heading-block title-center nobottomborder page-section">
+					<h2>Kenali Lebih Dekat API BCA</h2>
+					<span>Bagaimana cara mengintegrasikan aplikasi Anda dengan sistem BCA??</span>
+				</div>
+
+				<div class="center">
+					<a href="https://developer.bca.co.id/documentation/" target="_blank" class="button button-xlarge button-rounded bottommargin document" id="document">Baca Dokumentasi</a>
+				</div>
+
+			</div>
+
+		</div>
+		
+		<section id="section-services" class="page-section">
+
+			<div class="heading-block center bottommargin-lg">
+				<h2>API</h2>
+			</div>
+
+			<div class="container clearfix">
+				
+				<div class="col_one_fourth nobottommargin">
+					<div class="feature-box fbox-center fbox-light fbox-plain ApiHome">
+						<div class="fbox-icon">
+							<img src="templates/img/transfer.png">
+						</div>
+						<h3>Transfer</h3>
+						<p id="ApiHome">Menyediakan fasilitas transfer antar rekening BCA yang praktis dan aman. Kini transfer semakin mudah dilakukan.</p>
+						<a href="https://developer.bca.co.id/api-catalog/apitester/tests/Banking-Corporates-Transfers/api#!/transfers/transferOnlineCorporate" class="button button-large button-border button-rounded home">Coba Sekarang</a>
+
+					</div>
+
+				</div>
+				
+				<div class="col_one_fourth nobottommargin">
+					<div class="feature-box fbox-center fbox-light fbox-plain ApiHome">
+						<div class="fbox-icon">
+							<img src="templates/img/inquirysaldo.png">
+						</div>
+						<h3>Informasi Saldo</h3>
+						<p id="ApiHome">Menyediakan informasi saldo rekening sehingga perencanaan transaksi finansial semakin mudah dan nyaman.</p>
+						<a href="https://developer.bca.co.id/api-catalog/apitester/tests/Banking-Corporates/api#!/corporates/inquirySaldo" class="button button-large button-border button-rounded home">Coba Sekarang</a>
+
+					</div>
+
+				</div>
+				
+				<div class="col_one_fourth nobottommargin">
+					<div class="feature-box fbox-center fbox-light fbox-plain ApiHome" id="mut">
+						<div class="fbox-icon">
+							<img src="templates/img/inquirymutasi.png">
+						</div>
+						<h3>Mutasi Rekening</h3>
+						<p id="ApiHome">Menyediakan informasi terkini seputar alur keluar masuk dana yang terdapat di rekening BCA.</p>
+						<a href="https://developer.bca.co.id/api-catalog/apitester/tests/Banking-Corporates/api#!/corporates/inquiryMutasi" class="button button-large button-border button-rounded home">Coba Sekarang</a>
+
+					</div>
+
+
+			</div>
+
+			<div class="col_one_fourth col_last nobottommargin">
+				<div class="feature-box fbox-center fbox-light fbox-plain ApiHome">
+					<div class="fbox-icon">
+						<img src="templates/img/foreignexchange.png">
+					</div>
+					<h3>Info Kurs</h3>
+					<p id="ApiHome">Menghadirkan informasi kurs BCA terkini untuk membantu perencanaan transaksi lintas mata uang.</p>
+					<a href="https://developer.bca.co.id/api-catalog/apitester/tests/General-Rate-Forex/api#!/forex/inquiryForexRate" class="button button-large button-border button-rounded home">Coba Sekarang</a>
+
+				</div>
+			</div>
+
+				<div class="clear"></div>
+
+			</div>
+
+		<div class="divider divider-center"></div>
+
+		<div class="center"><a href="https://developer.bca.co.id/apis" class="button button-large button-border button-rounded">Lihat API Lain</a></div>
+
+		</section>
+
+
+
+			<div class="clear"></div>
+
+
+		
+
+		<div class="promo promo-dark promo-flat promo-full topmargin footer-stick" style="background-color:#0D5CAB;">
+				<div class="container clearfix">
+					<h3>Sandbox API</h3>
+					<span>Cara cepat untuk mencoba mengintegrasikan Aplikasi Anda dengan sistem BCA</span>
+
+					<a href="https://developer.bca.co.id/sign-in" class="button button-rounded button-reveal button-large button-white button-light tright"><i class="icon-line-arrow-right"></i><span>Coba Sekarang</span></a>
+				</div>
+		</div>
+
+	</div> <!--Content Warp-->
+
+
+
+</section><!-- #content end -->
+		
+		<!-- Footer
+============================================= -->
+
+<footer id="footer" class="dark">
+
+
+	<!-- Copyrights
+	============================================= -->
+	<div id="copyrights">
+
+		<div class="container clearfix">
+
+			<div class="col_half">
+				<span style="color:white;">Copyrights © 2016 PT Bank Central Asia Tbk, All Rights Reserved<br>
+				</span><div class="copyright-links" style="color:white;"><a href="https://developer.bca.co.id/term-of-use" style="color:white;">Terms of Use</a> | <a href="https://developer.bca.co.id/faq" style="color:white;">FAQ</a> | <a href="https://developer.bca.co.id/contact-us" style="color:white;">Contact Us</a></div>
+			</div>
+
+			<div class="col_half col_last tright" style="color:white;">
+
+				<div class="clear"></div>
+
+				<i class="icon-envelope2"></i> api_support@bca.co.id
+			</div>
+
+		</div>
+
+	</div><!-- #copyrights end -->
+
+</footer><!-- #footer end -->	
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<!--<div id="gotoTop" class="icon-angle-up"></div>-->
+
+	<!-- External JavaScripts
+	============================================= -->
+						<script type="text/javascript" src="templates/js/plugins.js"></script>
+				
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="templates/js/functions.js"></script>
+
+
+</body></html>
